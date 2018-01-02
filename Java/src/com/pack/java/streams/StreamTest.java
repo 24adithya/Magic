ffd7cf1b757ab0d2df42c10b230f1007cee541bf
Adithya Narayan <Aditya.Narayana@fisglobal.com>
2018-01-02 18:58:24 +0530

    test change password

--- Java/src/com/pack/java/streams/StreamTest.java
+++ Java/src/com/pack/java/streams/StreamTest.java
@@ -82,9 +82,9 @@ public class StreamTest {
 		employeeAttrs.put("Sravya", false);
 		employeeAttrs.put("Vidyasagar", false);
 		
-		System.out.println("Test one more");
 		
-//		employeeAttrs.values().stream().filter(t -> true == t).TODO
+		
+		employeeAttrs.values().parallelStream().filter(t -> true == t);
 	}
 	
 	private void testCombinedData() {
